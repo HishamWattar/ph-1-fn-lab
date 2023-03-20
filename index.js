fc12c5a85c403eadb081ecb8daa77c26b6bc8792
hisham <h.wattar@netro.xyz>
2023-03-20 23:33:11 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  if (blocks > 42) {
+    return blocks - 42;
+  } else {
+    return 42 - blocks;
+  }
+}
+
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (start < end) {
+    return (end - start) * 264;
+  } else {
+    return (start - end) * 264;
+  }
+}
+
+function calculatesFarePrice(start, end) {
+  const distance = distanceTravelledInFeet(start, end);
+  let fare;
+
+  if (distance <= 400) {
+    fare = 0;
+  } else if (distance > 400 && distance <= 2000) {
+    fare = (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    fare = 25;
+  } else {
+    return 'cannot travel that far';
+  }
+
+  return fare;
+}
